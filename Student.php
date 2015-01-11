ebbfadbcb53bdb6785955a71b89015a82d4b03c8
slee-colonel <slee_colonel@hotmail.com>
2015-01-11 13:37:12 -0800

    Added comments into Student.php

--- Student.php
+++ Student.php
@@ -9,9 +9,14 @@
 /**
  * Description of Student
  *
- * @author Sanders
+ * @author Sanders Lee
  */
 class Student {
+    
+    /*
+     * names the variables held by the Student class, has surname, first name,
+     * emails, and grades
+     */
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -19,27 +24,41 @@ class Student {
         $this->grades = array();
     }
     
+    /*
+     * function for adding an email, takes an email type (as the reference key
+     * within the array) and the actual email
+     */
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
 
+    /*
+     * function for adding a grade
+     */    
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    /*
+     * takes previously stored grades and returns an average from them
+     */
     function average() {
         $total = 0;
-        foreach ($this->grades as $value)
+        foreach ($this->grades as $value)   // goes through grades using foreach
             $total += $value;
         return $total / count($this->grades);
     }
     
+    /*
+     * returns a simply formatted string describing a student's details
+     * including first name, surname, average grade, and emails.
+     */
     function toString() {
-        $result = $this->first_name . ' ' . $this->surname;
-        $result .= ' ('.$this->average().")\n";
-        foreach($this->emails as $which=>$what)
-            $result .= $which . ': '. $what. "\n";
-        $result .= "\n";
-        return '<pre>'.$result.'</pre>';
+        $result = $this->first_name . ' ' . $this->surname; // full name
+        $result .= ' ('.$this->average().")\n"; // grade average
+        foreach($this->emails as $which=>$what) // emails sorted by email-type
+            $result .= $which . ': '. $what. "\n";  // in descending order
+        $result .= "\n";    // always attach a newline character at the end
+        return '<pre>'.$result.'</pre>';    // returns string enclosed by tags
     }
 }
